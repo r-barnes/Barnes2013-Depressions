f8c492c2193c41bb9efbd46627903b7e52c18f3e
Richard Barnes <rbarnes@umn.edu>
2013-03-27 13:00:25 -0500

    Added priority_flood_epsilon

--- src/priority_flood.hpp
+++ src/priority_flood.hpp
@@ -2,6 +2,7 @@
 #define pit_fill_include
 #include "data_structures.h"
 #include <queue>
+#include <limits>
 
 //barnes_priority_flood
 /**
@@ -86,6 +87,91 @@ void barnes_priority_flood(array2d<T> &elevations){
 }
 
 
+//priority_flood_epsilon
+/**
+  @brief  Assigns every cell an elevation which guarantees drainage
+  @author Richard Barnes (rbarnes@umn.edu)
+
+    This version of Priority-Flood starts on the edges of the DEM and then
+    works its way inwards using a priority queue to determine the lowest cell
+    which has a path to the edge. The neighbours of this cell are added to the
+    priority queue if they are higher. If they are lower, then their elevation
+    is increased to ensure that they have a drainage path and they are added to
+    a "pit" queue which is used to flood pits. Cells which are higher than a
+    pit being filled are added to the priority queue. In this way, pits are
+    filled without incurring the expense of the priority queue.
+
+  @param[in,out]  &elevations   A grid of cell elevations
+*/
+template <class T>
+void priority_flood_epsilon(array2d<T> &elevations){
+  grid_cellz_pq open;
+  std::queue<grid_cellz> pit;
+  bool_2d closed;
+  unsigned long processed_cells=0;
+  unsigned long pitc=0;
+  ProgressBar progress;
+
+  diagnostic("\n###Barnes Flood\n");
+  diagnostic("Setting up boolean flood array matrix...");
+  closed.copyprops(elevations);
+  closed.init(false);
+  diagnostic("succeeded.\n");
+
+  diagnostic_arg("The priority queue will require approximately %ldMB of RAM.\n",(elevations.width()*2+elevations.height()*2)*((long)sizeof(grid_cellz))/1024/1024);
+  diagnostic("Adding cells to the priority queue...");
+  for(int x=0;x<elevations.width();x++){
+    open.push_cell(x,0,elevations(x,0) );
+    open.push_cell(x,elevations.height()-1,elevations(x,elevations.height()-1) );
+    closed(x,0)=true;
+    closed(x,elevations.height()-1)=true;
+  }
+  for(int y=1;y<elevations.height()-1;y++){
+    open.push_cell(0,y,elevations(0,y)  );
+    open.push_cell(elevations.width()-1,y,elevations(elevations.width()-1,y) );
+    closed(0,y)=true;
+    closed(elevations.width()-1,y)=true;
+  }
+  diagnostic("succeeded.\n");
+
+  diagnostic("%%Performing the Barnes Flood...\n");
+  progress.start( elevations.width()*elevations.height() );
+  while(open.size()>0 || pit.size()>0){
+    grid_cellz c;
+    if(pit.size()>0 && open.size()>0 && open.top().z==pit.front().z){
+      c=open.top();
+      open.pop();
+    } else if(pit.size()>0){
+      c=pit.front();
+      pit.pop();
+    } else {
+      c=open.top();
+      open.pop();
+    }
+    processed_cells++;
+
+    for(int n=1;n<=8;n++){
+      int nx=c.x+dx[n];
+      int ny=c.y+dy[n];
+      if(!elevations.in_grid(nx,ny)) continue;
+      if(closed(nx,ny)) 
+        continue;
+
+      closed(nx,ny)=true;
+      if(elevations(nx,ny)<nextafterf(c.z,std::numeric_limits<float>::infinity())){
+        if(elevations(nx,ny)<c.z){
+          ++pitc;
+          elevations(nx,ny)=nextafterf(c.z,std::numeric_limits<float>::infinity());
+        }
+        pit.push(grid_cellz(nx,ny,c.z));
+      } else
+        open.push_cell(nx,ny,elevations(nx,ny));
+    }
+    progress.update(processed_cells);
+  }
+  diagnostic_arg(SUCCEEDED_IN,progress.stop());
+  diagnostic_arg("%ld cells processed. %ld in pits.\n",processed_cells,pitc);
+}
 
 
 
