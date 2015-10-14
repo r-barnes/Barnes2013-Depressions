68c4a71f7911d37242a26482181ca7231930a358
Richard Barnes <rbarnes@umn.edu>
2015-10-14 16:20:27 -0700

    Switch to cells of width and height 1

--- awi/gen_surface.py
+++ awi/gen_surface.py
@@ -9,7 +9,7 @@ def writeArrayToArcGrid(filename,arr):
   arr[np.isnan(arr)] = -9999
   arr                = np.flipud(arr)
   fout               = open(filename,'wb')
-  headerstring       = bytes('NCOLS %d\nNROWS %d\nXLLCENTER %f\nYLLCENTER %f\nCELLSIZE %f\nNODATA_value %f\n' % (arr.shape[1], arr.shape[0], -4.2000, 53.2200, 3,-9999), 'UTF-8')
+  headerstring       = bytes('NCOLS %d\nNROWS %d\nXLLCENTER %f\nYLLCENTER %f\nCELLSIZE %f\nNODATA_value %f\n' % (arr.shape[1], arr.shape[0], -4.2000, 53.2200, 1,-9999), 'UTF-8')
   fout.write(headerstring)
   np.savetxt(fout,arr,'%5.5f')
 
@@ -17,12 +17,20 @@ if len(sys.argv)!=2:
   print("Syntax: {0} <show/noshow>".format(sys.argv[0]))
   print("\tshow   = Show surfaces as they are generated")
   print("\tnoshow = Don't show surfaces")
+  sys.exit(-1)
+
+lbound = -3
+ubound = 3
+npts   = 50.0
 
 #Generate test surface z(x,y) = sin(x cos(y))
-x = np.linspace(-3, 3, 50)
-y = np.linspace(-3, 3, 50)
-xx, yy = np.meshgrid(x, y)
-z = np.sin(xx*np.cos(yy))
+x            = np.linspace(lbound, ubound, npts)
+y            = np.linspace(lbound, ubound, npts)
+xx, yy       = np.meshgrid(x, y)
+z            = np.sin(xx*np.cos(yy))
+zmod         = -(10*yy)**2+1
+zmod[zmod<0] = 0
+z           += zmod
 
 #Show test surface
 if sys.argv[1]=="show":
